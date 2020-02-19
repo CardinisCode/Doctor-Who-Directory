3299878c699e2dbc6a141eb6fcd4afb59d86c709
Andi Folgado <ac.folgado@gmail.com>
2020-02-19 16:55:05 +0000

    A Dictionary containing all the episodes for season 9.

---
+++ ninthdoctor.py
@@ -0,0 +1,22 @@
+#This dictionary will include all the episodes, including bonus episodes, with their names and in the order you should watch them. 
+
+episodes = {
+    1: {"Actor":"Christopher Eccleston","Name": "Rose", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Keith Boak", "Written by":"Russell T Davies"},
+    2: {"Actor":"Christopher Eccleston","Name": "The end of the world", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Euros Lyn", "Written by":"Russell T Davies"},
+    3: {"Actor":"Christopher Eccleston","Name": "The unquiet dead", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Euros Lyn", "Written by":"Mark Gatiss"},
+    4: {"Actor":"Christopher Eccleston","Name": "Aliens of London", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Keith Boak", "Written by":"Russell T Davies"},
+    5: {"Actor":"Christopher Eccleston","Name": "World War Three", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Keith Boak", "Written by":"Russell T Davies"},
+    6: {"Actor":"Christopher Eccleston","Name": "Dalek", "Companions":["Billie Piper (Rose Tyler)", "Bruno Langley (Adam Mitchell)"], "Directed by":"Joe Ahearne", "Written by":"Robert Shearman"},
+    7: {"Actor":"Christopher Eccleston","Name": "The Long Game", "Companions":["Billie Piper (Rose Tyler)", "Bruno Langley (Adam Mitchell)"], "Directed by":"Brian Grant", "Written by":"Russell T Davies"},
+    8: {"Actor":"Christopher Eccleston","Name": "Father's Day", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Joe Ahearne", "Written by":"Paul Cornell"},
+    9: {"Actor":"Christopher Eccleston","Name": "The Empty Child", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"James Hawes", "Written by":"Steven Moffat"},
+    10: {"Actor":"Christopher Eccleston","Name": "The Doctor Dances", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"James Hawes", "Written by":"Steven Moffat"},
+    11: {"Actor":"Christopher Eccleston","Name": "Boom Town", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"Joe Ahearne", "Written by":"Russell T Davies"},
+    12: {"Actor":"Christopher Eccleston","Name": "Bad Wolf", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"Joe Ahearne", "Written by":"Russell T Davies"},
+    13: {"Actor":"Christopher Eccleston","Name": "The Parting of the Ways", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"Joe Ahearne", "Written by":"Russell T Davies"}
+}
+
+lead_actor = episodes[1]["Actor"]
+main_companion = episodes[1]["Companions"]
+print("In Season 9", lead_actor, "played the lead role as the Doctor, with", main_companion, "by his side.")
+
