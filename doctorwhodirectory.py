9e8864937dfeda57002f00353304046e1f160a8c
Andi Folgado <ac.folgado@gmail.com>
2020-02-20 10:14:07 +0000

    Update

---
+++ doctorwhodirectory.py
@@ -0,0 +1,82 @@
+#This dictionary will include all the episodes, including bonus episodes, with their names and in the order you should watch them. 
+
+season_nine_episodes = {
+    1: {"Actor":"Christopher Eccleston","Name": "Rose", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Keith Boak", "Written by":"Russell T Davies", "Featuring":""},
+    2: {"Actor":"Christopher Eccleston","Name": "The end of the world", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Euros Lyn", "Written by":"Russell T Davies", "Featuring":""},
+    3: {"Actor":"Christopher Eccleston","Name": "The unquiet dead", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Euros Lyn", "Written by":"Mark Gatiss", "Featuring":""},
+    4: {"Actor":"Christopher Eccleston","Name": "Aliens of London", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Keith Boak", "Written by":"Russell T Davies", "Featuring":""},
+    5: {"Actor":"Christopher Eccleston","Name": "World War Three", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Keith Boak", "Written by":"Russell T Davies", "Featuring":""},
+    6: {"Actor":"Christopher Eccleston","Name": "Dalek", "Companions":["Billie Piper (Rose Tyler)", "Bruno Langley (Adam Mitchell)"], "Directed by":"Joe Ahearne", "Written by":"Robert Shearman", "Featuring":""},
+    7: {"Actor":"Christopher Eccleston","Name": "The Long Game", "Companions":["Billie Piper (Rose Tyler)", "Bruno Langley (Adam Mitchell)"], "Directed by":"Brian Grant", "Written by":"Russell T Davies", "Featuring":""},
+    8: {"Actor":"Christopher Eccleston","Name": "Father's Day", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Joe Ahearne", "Written by":"Paul Cornell", "Featuring":""},
+    9: {"Actor":"Christopher Eccleston","Name": "The Empty Child", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"James Hawes", "Written by":"Steven Moffat", "Featuring":""},
+    10: {"Actor":"Christopher Eccleston","Name": "The Doctor Dances", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"James Hawes", "Written by":"Steven Moffat", "Featuring":""},
+    11: {"Actor":"Christopher Eccleston","Name": "Boom Town", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"Joe Ahearne", "Written by":"Russell T Davies", "Featuring":""},
+    12: {"Actor":"Christopher Eccleston","Name": "Bad Wolf", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"Joe Ahearne", "Written by":"Russell T Davies", "Featuring":""},
+    13: {"Actor":"Christopher Eccleston","Name": "The Parting of the Ways", "Companions":["Billie Piper (Rose Tyler)", "John Barrowman (Jack Harkness)"], "Directed by":"Joe Ahearne", "Written by":"Russell T Davies", "Featuring":""}
+}
+
+season_nine_lead = season_nine_episodes[1]["Actor"]
+season_nine_companion = season_nine_episodes[1]["Companions"]
+print("In Season 9", season_nine_lead, "played the lead role as the Doctor, with", season_nine_companion, "by his side.")
+
+season_ten_episodes = {
+    0: {"Actor":"David Tennant","Name": "The Christmas Invasion", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"James Hawes", "Written by":"Russell T Davies", "Featuring":""},
+    1: {"Actor":"David Tennant","Name": "New Earth", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"James Hawes", "Written by":"Russell T Davies", "Featuring":""},
+    2: {"Actor":"David Tennant","Name": "Tooth and Claw", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Euros Lyn", "Written by":"Russell T Davies", "Featuring":""},
+    3: {"Actor":"David Tennant","Name": "School Reunion", "Companions":["Billie Piper (Rose Tyler)", "Noel Clarke (Mickey Smith)"], "Directed by":"James Hawes", "Written by":"Toby Whithouse", "Featuring":""},
+    4: {"Actor":"David Tennant","Name": "The Girl in the Fireplace", "Companions":["Billie Piper (Rose Tyler)", "Noel Clarke (Mickey Smith)"], "Directed by":"Keith Boak", "Written by":"Steven Moffat", "Featuring":""},
+    5: {"Actor":"David Tennant","Name": "Rise of the Cybermen", "Companions":["Billie Piper (Rose Tyler)", "Noel Clarke (Mickey Smith)"], "Directed by":"Graeme Harper", "Written by":"Tom MacRae", "Featuring":""},
+    6: {"Actor":"David Tennant","Name": "The Age of Steel", "Companions":["Billie Piper (Rose Tyler)", "Noel Clarke (Mickey Smith)"], "Directed by":"Graeme Harper", "Written by":"Tom MacRae", "Featuring":""},
+    7: {"Actor":"David Tennant","Name": "The Idiot's Lantern", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Euros Lyn", "Written by":"Mark Gatiss", "Featuring":""},
+    8: {"Actor":"David Tennant","Name": "The Impossible Planet", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"James Strong", "Written by":"Matt Jones", "Featuring":""},
+    9: {"Actor":"David Tennant","Name": "The Satan Pit", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"James Strong", "Written by":"Matt Jones", "Featuring":""},
+    10: {"Actor":"David Tennant","Name": "Love & Monsters", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Dan Zeff", "Written by":"Russell T Davies", "Featuring":""},
+    11: {"Actor":"David Tennant","Name": "Fear Her", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Euros Lyn", "Written by":"Matthew Graham", "Featuring":""},
+    12: {"Actor":"David Tennant","Name": "Army of Ghosts", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Graeme Harper", "Written by":"Russell T Davies", "Featuring":""},
+    13: {"Actor":"David Tennant","Name": "Doomsday", "Companions":"Billie Piper (Rose Tyler)", "Directed by":"Graeme Harper", "Written by":"Russell T Davies", "Featuring":""}
+}
+
+season_eleven_episodes = {
+    0: {"Actor":"David Tennant","Name": "The Runaway Bride", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Euros Lyn", "Written by":"Russell T Davies", "Featuring":""},
+    1: {"Actor":"David Tennant","Name": "Smith and Jones", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"Charles Palmer", "Written by":"Russell T Davies", "Featuring":""},
+    2: {"Actor":"David Tennant","Name": "The Shakespeare Code", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"Charles Palmer", "Written by":"Gareth Roberts", "Featuring":""},
+    3: {"Actor":"David Tennant","Name": "Gridlock", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"Richard Clark", "Written by":"Russell T Davies", "Featuring":""},
+    4: {"Actor":"David Tennant","Name": "Daleks in Manhattan", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"James Strong", "Written by":"Helen Raynor", "Featuring":""},
+    5: {"Actor":"David Tennant","Name": "Evolution of the Daleks", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"James Strong", "Written by":"Helen Raynor", "Featuring":""},
+    6: {"Actor":"David Tennant","Name": "The Lazarus Experiment", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"Richard Clark", "Written by":"Stephen Greenhorn", "Featuring":""},
+    7: {"Actor":"David Tennant","Name": "42", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"Graeme Harper", "Written by":"Chris Chibnall"},
+    8: {"Actor":"David Tennant","Name": "Human Nature", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"Charles Palmer", "Written by":"Paul Cornell"},
+    9: {"Actor":"David Tennant","Name": "The Family of Blood", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"Charles Palmer", "Written by":"Paul Cornell", "Featuring":""},
+    10: {"Actor":"David Tennant","Name": "Blink", "Companions":"Freema Agyeman (Martha Jones)", "Directed by":"Hettie MacDonald", "Written by":"Hettie MacDonald", "Featuring":""},
+    11: {"Actor":"David Tennant","Name": "Utopia", "Companions":["Freema Agyeman (Martha Jones)", "John Barrowman (Jack Harkness)"], "Directed by":"Graeme Harper", "Written by":"Russell T Davies", "Featuring":""},
+    12: {"Actor":"David Tennant","Name": "The Sound of Drums", "Companions":["Freema Agyeman (Martha Jones)", "John Barrowman (Jack Harkness)"], "Directed by":"Colin Teague", "Written by":"Russell T Davies", "Featuring":""},
+    13: {"Actor":"David Tennant","Name": "Last of the Time Lords", "Companions":["Freema Agyeman (Martha Jones)", "John Barrowman (Jack Harkness)"], "Directed by":"Graeme Harper", "Written by":"Russell T Davies", "Featuring":""}
+}
+#Currently working on season 12 
+season_twelve_episodes = {
+    0: {"Actor":"David Tennant","Name": "Voyage of the Damned", "Companions":"Kylie Minogue (Astrid Peth)", "Directed by":"James Strong", "Written by":"Russell T Davies", "Featuring":""},
+    1: {"Actor":"David Tennant","Name": "Partners in Crime", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"James Strong", "Written by":"Russell T Davies", "Featuring":""},
+    2: {"Actor":"David Tennant","Name": "The Fires of Pompeii", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Colin Teague", "Written by":"James Moran", "Featuring":"Peter Capaldi – Caecilius"},
+    3: {"Actor":"David Tennant","Name": "Planet of the Ood", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Graeme Harper", "Written by":"Keith Temple", "Featuring":""},
+    4: {"Actor":"David Tennant","Name": "The Sontaran Stratagem", "Companions":["Catherine Tate (Donna Noble)","Freema Agyeman (Martha Jones)"], "Directed by":"Douglas Mackinnon", "Written by":"Helen Raynor", "Featuring":""},
+    5: {"Actor":"David Tennant","Name": "The Poison Sky", "Companions":["Catherine Tate (Donna Noble)","Freema Agyeman (Martha Jones)"], "Directed by":"Douglas Mackinnon", "Written by":"Helen Raynor", "Featuring":""},
+    6: {"Actor":"David Tennant","Name": "The Doctor's Daughter", "Companions":["Catherine Tate (Donna Noble)","Freema Agyeman (Martha Jones)"], "Directed by":"Alice Troughton", "Written by":"Stephen Greenhorn", "Featuring":"Georgia Moffett – Jenny"},
+    7: {"Actor":"David Tennant","Name": "The Unicorn and the Wasp", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Graeme Harper", "Written by":"Chris Chibnall", "Featuring":""},
+    8: {"Actor":"David Tennant","Name": "Silence in the Library", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Charles Palmer", "Written by":"Paul Cornell", "Featuring":""},
+    9: {"Actor":"David Tennant","Name": "Forest of the Dead", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Charles Palmer", "Written by":"Paul Cornell", "Featuring":""},
+    10: {"Actor":"David Tennant","Name": "Midnight", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Hettie MacDonald", "Written by":"Hettie MacDonald", "Featuring":""},
+    11: {"Actor":"David Tennant","Name": "Turn Left", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Graeme Harper", "Written by":"Russell T Davies", "Featuring":""},
+    12: {"Actor":"David Tennant","Name": "The Stolen Earth", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Colin Teague", "Written by":"Russell T Davies", "Featuring":""},
+    13: {"Actor":"David Tennant","Name": "Journey's End", "Companions":"Catherine Tate (Donna Noble)", "Directed by":"Graeme Harper", "Written by":"Russell T Davies", "Featuring":""}
+
+	Stephen Greenhorn
+
+
+
+
+}
+
+#35:2 PC + 4 sheets 
+#VS
+#42.50
